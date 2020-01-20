1e53b1bb3b81ca7a0891e573ba8c5e8d597a85fb
OlhaLu <olhalutsyshyna@gmail.com>
2020-01-20 17:15:51 +0200

    Added resultPage markup

--- src/pages/ResultPage/ResultPage.js
+++ src/pages/ResultPage/ResultPage.js
@@ -1,10 +1,26 @@
 import React from 'react';
-// import styles from './ResultPage.module.css';
+import img from '../../assets/images/cats/2.png';
+import styles from './ResultPage.module.css';
 
 const ResultPage = () => {
   return (
-    <div>
-      <h2>ResultPage</h2>
+    <div className={styles.section}>
+      <h2 className={styles.main_header}>Результаты</h2>
+      <h3 className={styles.test_title}>[ теория тестирования_ ]</h3>
+
+      <div className={styles.result_bar}>! For result_progress_bar !</div>
+      <img
+        className={styles.img}
+        src={img}
+        width="180"
+        height="200"
+        alt="cat"
+      />
+      <h2 className={styles.main_header}>Неплохой результат!</h2>
+      <h3 className={styles.test_info}>Но тебе нужно доучить материалы.</h3>
+      <button className={styles.button} type="button">
+        Пройти еще раз
+      </button>
     </div>
   );
 };
