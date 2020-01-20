e31a81ecfbd32b5f26518e164e77d38af00471a7
OlhaLu <olhalutsyshyna@gmail.com>
2020-01-20 22:48:49 +0200

    Added mobile/tablet

--- src/pages/ResultPage/ResultPage.js
+++ src/pages/ResultPage/ResultPage.js
@@ -4,24 +4,22 @@ import styles from './ResultPage.module.css';
 
 const ResultPage = () => {
   return (
-    <div className={styles.section}>
-      <h2 className={styles.main_header}>Результаты</h2>
-      <h3 className={styles.test_title}>[ теория тестирования_ ]</h3>
+    <>
+      <div className={styles.section}>
+        <h2 className={styles.main_header}>Результаты</h2>
+        <h3 className={styles.test_title}>[ теория тестирования_ ]</h3>
 
-      <div className={styles.result_bar}>! For result_progress_bar !</div>
-      <img
-        className={styles.img}
-        src={img}
-        width="180"
-        height="200"
-        alt="cat"
-      />
-      <h2 className={styles.main_header}>Неплохой результат!</h2>
-      <h3 className={styles.test_info}>Но тебе нужно доучить материалы.</h3>
-      <button className={styles.button} type="button">
-        Пройти еще раз
-      </button>
-    </div>
+        <div className={styles.result_bar}>! For result_progress_bar !</div>
+        <div className={styles.img}>
+          <img src={img} width="180" height="200" alt="cat" />
+        </div>
+        <h2 className={styles.main_header}>Неплохой результат!</h2>
+        <h3 className={styles.test_info}>Но тебе нужно доучить материалы.</h3>
+        <button className={styles.button} type="button">
+          Пройти еще раз
+        </button>
+      </div>
+    </>
   );
 };
 
