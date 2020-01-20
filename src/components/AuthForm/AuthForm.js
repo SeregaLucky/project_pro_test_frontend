094895f59386498c4afc74d2b9d12f8c6fa06981
IryArkhy <37odri@gmail.com>
2020-01-20 19:10:36 +0200

    1) markup - done; 2) styles -done: mobile, tablet,desktop

--- src/components/AuthForm/AuthForm.js
+++ src/components/AuthForm/AuthForm.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import googleIcon from '../../assets/icons/google-auth.png';
+import styles from './authForm.module.css';
 
-const AuthForm = () => {
-  return (
-    <div>
-      <h2>AuthForm</h2>
-    </div>
-  );
-};
+class AuthForm extends Component {
+  render() {
+    return (
+      <div className={styles.formWrapper}>
+        <p className={styles.formText}>
+          Для авторизации можете использовать Google Account:
+        </p>
+
+        {/*  eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a href="#" className={styles.googleSignUpButton}>
+          <div>
+            <img alt="google" src={googleIcon} />
+            <span>Google</span>
+          </div>
+        </a>
+        <p className={styles.formText}>
+          Или войдите в приложение используя e-mail и пароль:
+        </p>
+        <form className={styles.signUpForm}>
+          <input
+            type="email"
+            name="email"
+            placeholder="E-mail"
+            autoComplete="on"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            autoComplete="on"
+          />
+        </form>
+        <div className={styles.formButtonWrapper}>
+          <button className={styles.formButton}>ВОЙТИ</button>
+          <button className={styles.formButton}>РЕГИСТРАЦИЯ</button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default AuthForm;
--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AuthForm from '../../components/AuthForm';
 // import styles from './AuthPage.module.css';
 
 const AuthPage = () => {
   return (
     <div>
       <h2>AuthPage</h2>
+      <AuthForm />
     </div>
   );
 };
