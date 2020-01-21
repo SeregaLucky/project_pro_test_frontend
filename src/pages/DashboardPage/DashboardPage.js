8cb638c7c9ced5f02d5bf63821f61a50b7c25dc3
sergeypoddubnii <sergey_pidd@ukr.net>
2020-01-21 19:26:45 +0200

    dashboardpage

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,10 +1,33 @@
 import React from 'react';
-// import styles from './DashboardPage.module.css';
+import styles from './DashboardPage.module.css';
+import { ReactComponent as Arrow } from '../../assets/icons/svg/arrow.svg';
 
 const DashboardPage = () => {
   return (
-    <div>
-      <h2>DashboardPage</h2>
+    <div className={styles.dashboardPageContainer}>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.titleContainer__title}>
+          [ Теория тестирования_ ]
+        </h1>
+        <button className={styles.titleContainer__btnEnd} type="button">
+          заглушка
+        </button>
+      </div>
+      <div className={styles.DashboardForm}>заглушка</div>
+      <div className={styles.btnContainer}>
+        <button className={styles.btnContainer__back} type="button">
+          <Arrow className={styles.btnContainer__arrow_back} />
+          <span className={styles.btnContainer__backText}>
+            Предыдущий вопрос
+          </span>
+        </button>
+        <button className={styles.btnContainer__forward} type="button">
+          <span className={styles.btnContainer__forwardText}>
+            Следующий вопрос
+          </span>
+          <Arrow className={styles.btnContainer__arrow_forward} />
+        </button>
+      </div>
     </div>
   );
 };
