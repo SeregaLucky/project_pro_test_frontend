059fc046181650cdad0139305e751d5849b959ab
OlhaLu <olhalutsyshyna@gmail.com>
2020-01-21 12:22:20 +0200

    fixed container size

--- src/pages/ResultPage/ResultPage.js
+++ src/pages/ResultPage/ResultPage.js
@@ -4,22 +4,22 @@ import styles from './ResultPage.module.css';
 
 const ResultPage = () => {
   return (
-    <>
-      <div className={styles.section}>
+    <section className={styles.section}>
+      <div className={styles.container}>
         <h2 className={styles.main_header}>Результаты</h2>
-        <h3 className={styles.test_title}>[ теория тестирования_ ]</h3>
+        <p className={styles.test_title}>[ теория тестирования_ ]</p>
 
         <div className={styles.result_bar}>! For result_progress_bar !</div>
         <div className={styles.img}>
           <img src={img} width="180" height="200" alt="cat" />
         </div>
         <h2 className={styles.main_header}>Неплохой результат!</h2>
-        <h3 className={styles.test_info}>Но тебе нужно доучить материалы.</h3>
+        <p className={styles.test_info}>Но тебе нужно доучить материалы.</p>
         <button className={styles.button} type="button">
           Пройти еще раз
         </button>
       </div>
-    </>
+    </section>
   );
 };
 
