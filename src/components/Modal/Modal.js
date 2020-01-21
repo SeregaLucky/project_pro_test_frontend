c04b0b2ceca86169ebcece2562f118000649c6f1
galat96 <halat0746@gmail.com>
2020-01-21 08:57:06 +0200

    Add modal component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,13 +1,49 @@
-import React from 'react';
-// import { createPortal } from 'react-dom';
-// import styles from './Modal.module.css';
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+// import T from 'prop-types';
 
-const Modal = () => {
-  return (
-    <div>
-      <h2>Modal</h2>
-    </div>
-  );
-};
+import styles from './Modal.module.css';
+
+const MODAL_ROOT = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  static propTypes = {
+    // onClose: T.func.isRequired,
+  };
+
+  render() {
+    return createPortal(
+      <div
+        className={styles.overlay}
+        ref={this.backdropRef}
+        // onClick={this.handleBackdropClick}
+        role="presentation"
+      >
+        <div className={styles.modal}>
+          {/* Children */}
+          <div class={styles.icon}></div>
+          <p className={styles.text}>Вы точно хотите выйти?</p>
+          <div className={styles.item}>
+            <button
+              className={styles.button}
+              type="button"
+              onClick={this.onClick}
+            >
+              Дa
+            </button>
+            <button
+              className={styles.button}
+              type="button"
+              onClick={this.onClick}
+            >
+              Нет
+            </button>
+          </div>
+        </div>
+      </div>,
+      MODAL_ROOT,
+    );
+  }
+}
 
 export default Modal;
