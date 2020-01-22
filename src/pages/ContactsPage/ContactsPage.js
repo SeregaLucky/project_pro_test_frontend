ca1c099ccbbf851e7363479fc1634fb1c21e3e6d
Manuilenkoart <manuilenkoart@gmail.com>
2020-01-22 22:15:23 +0200

    contacts_page

--- src/pages/ContactsPage/ContactsPage.js
+++ src/pages/ContactsPage/ContactsPage.js
@@ -4,34 +4,51 @@ import styles from './ContactsPage.module.css';
 const ContactsPage = () => {
   return (
     <section className={styles.wrapper}>
-      <h2 className={styles.mainTitle}>Наша команда</h2>
-
-      <ul className={styles.contactsListItemContainer}>
-        <li className={styles.contactsListItem}>
-          <img src="http://via.placeholder.com/280x245" alt="img" />
-          <p>Имя фамилия</p>
-          <p>доолжность</p>
-          <p>Lorem dvsdvs dbhdbs </p>
-        </li>
-        <li className={styles.contactsListItem}>
-          <img src="http://via.placeholder.com/280x245" alt="img" />
-          <p>Имя фамилия</p>
-          <p>доолжность</p>
-          <p>Lorem dvsdvs dbhdbs </p>
-        </li>
-        <li className={styles.contactsListItem}>
-          <img src="http://via.placeholder.com/280x245" alt="img" />
-          <p>Имя фамилия</p>
-          <p>доолжность</p>
-          <p>Lorem dvsdvs dbhdbs </p>
-        </li>
-        <li className={styles.contactsListItem}>
-          <img src="http://via.placeholder.com/280x245" alt="img" />
-          <p>Имя фамилия</p>
-          <p>доолжность</p>
-          <p>Lorem dvsdvs dbhdbs </p>
-        </li>
-      </ul>
+      <div className={styles.container}>
+        <h2 className={styles.mainTitle}>Наша команда</h2>
+        <ul className={styles.contactsListItemContainer}>
+          <li className={styles.contactsListItem}>
+            <figure>
+              <img src="http://via.placeholder.com/280x245" alt="img" />
+              <figcaption>
+                <p>Имя фамилия</p>
+                <p>доолжность</p>
+                <p>Lorem dvsdvs dbhdbs </p>
+              </figcaption>
+            </figure>
+          </li>
+          <li className={styles.contactsListItem}>
+            <figure>
+              <img src="http://via.placeholder.com/280x245" alt="img" />
+              <figcaption>
+                <p>Имя фамилия</p>
+                <p>доолжность</p>
+                <p>Lorem dvsdvs dbhdbs </p>
+              </figcaption>
+            </figure>
+          </li>
+          <li className={styles.contactsListItem}>
+            <figure>
+              <img src="http://via.placeholder.com/280x245" alt="img" />
+              <figcaption>
+                <p>Имя фамилия</p>
+                <p>доолжность</p>
+                <p>Lorem dvsdvs dbhdbs </p>
+              </figcaption>
+            </figure>
+          </li>
+          <li className={styles.contactsListItem}>
+            <figure>
+              <img src="http://via.placeholder.com/280x245" alt="img" />
+              <figcaption>
+                <p>Имя фамилия</p>
+                <p>доолжность</p>
+                <p>Lorem dvsdvs dbhdbs </p>
+              </figcaption>
+            </figure>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
