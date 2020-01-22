5e7bc8d7e0768ef7daf15efb9f717aed58f5c575
sergeypoddubnii <sergey_pidd@ukr.net>
2020-01-22 19:42:46 +0200

    dashboardpage fix

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -4,29 +4,31 @@ import { ReactComponent as Arrow } from '../../assets/icons/svg/arrow.svg';
 
 const DashboardPage = () => {
   return (
-    <div className={styles.dashboardPageContainer}>
-      <div className={styles.titleContainer}>
-        <h1 className={styles.titleContainer__title}>
-          [ Теория тестирования_ ]
-        </h1>
-        <button className={styles.titleContainer__btnEnd} type="button">
-          заглушка
-        </button>
-      </div>
-      <div className={styles.DashboardForm}>заглушка</div>
-      <div className={styles.btnContainer}>
-        <button className={styles.btnContainer__back} type="button">
-          <Arrow className={styles.btnContainer__arrow_back} />
-          <span className={styles.btnContainer__backText}>
-            Предыдущий вопрос
-          </span>
-        </button>
-        <button className={styles.btnContainer__forward} type="button">
-          <span className={styles.btnContainer__forwardText}>
-            Следующий вопрос
-          </span>
-          <Arrow className={styles.btnContainer__arrow_forward} />
-        </button>
+    <div className={styles.dashboardPage}>
+      <div className={styles.dashboardPageContainer}>
+        <div className={styles.titleContainer}>
+          <h2 className={styles.titleContainer__title}>
+            [ Теория тестирования_ ]
+          </h2>
+          <button className={styles.titleContainer__btnEnd} type="button">
+            заглушка
+          </button>
+        </div>
+        <div className={styles.dashboardForm}>заглушка</div>
+        <div className={styles.btnContainer}>
+          <button className={styles.btnContainer__back} type="button">
+            <Arrow className={styles.btnContainer__arrow_back} />
+            <span className={styles.btnContainer__backText}>
+              Предыдущий вопрос
+            </span>
+          </button>
+          <button className={styles.btnContainer__forward} type="button">
+            <span className={styles.btnContainer__forwardText}>
+              Следующий вопрос
+            </span>
+            <Arrow className={styles.btnContainer__arrow_forward} />
+          </button>
+        </div>
       </div>
     </div>
   );
