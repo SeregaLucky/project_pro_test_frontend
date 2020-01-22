45a7feaabef2e909f7d688493a449d03ad133904
Denys Plietnikov <givisimo@gmail.com>
2020-01-22 15:14:12 +0200

    propTypes add in HeaderUserInfo

--- src/components/HeaderUserInfo/HeaderUserInfo.js
+++ src/components/HeaderUserInfo/HeaderUserInfo.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import T from 'prop-types';
 import styles from './HeaderUserInfo.module.css';
 
 const HeaderUserInfo = ({ isMobile }) => {
   return isMobile === true ? (
     <>
       <div className={styles.HeaderUserContainer}>
-        <a href='#'  className={styles.HeaderUserLink}>
+        <a href="#" className={styles.HeaderUserLink}>
           <p className={styles.HeaderUserLetter}>Д</p>
         </a>
       </div>
     </>
   ) : (
     <div className={styles.HeaderUserContainer}>
-      <a href='#'  className={styles.HeaderUserLink}>
+      <a href="#" className={styles.HeaderUserLink}>
         <p className={styles.HeaderUserLetter}>Д</p>
         <p className={styles.HeaderUserName}>Дмитрий</p>
       </a>
     </div>
   );
+};
+
+HeaderUserInfo.propTypes = {
+  isMobile: T.bool.isRequired,
 };
 
 export default HeaderUserInfo;
