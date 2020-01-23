20801bff58e3efcf27767f1bb736b9603d08602d
Peter Mazuryk <petermazuryk1984@gmail.com>
2020-01-24 00:05:14 +0200

    Add DashboardFormInput component and layout

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import DashboardFormInput from '../../components/DashboardFormInput/DashboardFormInput';
 // import styles from './DashboardPage.module.css';
 
 const DashboardPage = () => {
   return (
     <div>
       <h2>DashboardPage</h2>
+      <DashboardFormInput />
     </div>
   );
 };
