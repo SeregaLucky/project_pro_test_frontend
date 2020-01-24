8b141c0957a13eff4252d052ef28152429177740
sergeypoddubnii <sergey_pidd@ukr.net>
2020-01-24 22:04:08 +0200

    fix_padding add_btn

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,10 +1,34 @@
 import React from 'react';
-// import styles from './DashboardPage.module.css';
+import styles from './DashboardPage.module.css';
+import { ReactComponent as Arrow } from '../../assets/icons/svg/arrow.svg';
+import Button from '../../components/Button';
 
 const DashboardPage = () => {
   return (
-    <div>
-      <h2>DashboardPage</h2>
+    <div className={styles.dashboardPage}>
+      <section className={styles.dashboardPageContainer}>
+        <div className={styles.titleContainer}>
+          <h2 className={styles.titleContainer__title}>
+            [ Теория тестирования_ ]
+          </h2>
+          <Button lable={'Завершить тест'} />
+        </div>
+        <div className={styles.dashboardForm}>заглушка</div>
+        <div className={styles.btnContainer}>
+          <button className={styles.btnContainer__back} type="button">
+            <Arrow className={styles.btnContainer__arrow_back} />
+            <span className={styles.btnContainer__backText}>
+              Предыдущий вопрос
+            </span>
+          </button>
+          <button className={styles.btnContainer__forward} type="button">
+            <span className={styles.btnContainer__forwardText}>
+              Следующий вопрос
+            </span>
+            <Arrow className={styles.btnContainer__arrow_forward} />
+          </button>
+        </div>
+      </section>
     </div>
   );
 };
