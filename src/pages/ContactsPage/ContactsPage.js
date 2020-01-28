c3a596c94fc8db1f17bfedc8c78de9e6b090a8c4
Manuilenkoart <manuilenkoart@gmail.com>
2020-01-28 20:17:53 +0200

    add items to contactsPage  rename cAntacts branch

--- src/pages/ContactsPage/ContactsPage.js
+++ src/pages/ContactsPage/ContactsPage.js
@@ -1,53 +1,22 @@
 import React from 'react';
 import styles from './ContactsPage.module.css';
-import CantactsListItem from '../../components/CantactsListItem/CantactsListItem';
-
+import ContactsListItem from '../../components/ContactsListItem/ContactsListItem';
+import projectTeam from './ProjectTeam';
 const ContactsPage = () => {
   return (
     <section className={styles.wrapper}>
       <div className={styles.container}>
         <h2 className={styles.mainTitle}>Наша команда</h2>
         <ul className={styles.contactsListItemContainer}>
-          <li className={styles.contactsListItem}>
-            <figure>
-              <img src="http://via.placeholder.com/280x245" alt="img" />
-              <figcaption>
-                <p>Имя фамилия</p>
-                <p>доолжность</p>
-                <p>Lorem dvsdvs dbhdbs </p>
-              </figcaption>
-            </figure>
-          </li>
-          <li className={styles.contactsListItem}>
-            <figure>
-              <img src="http://via.placeholder.com/280x245" alt="img" />
-              <figcaption>
-                <p>Имя фамилия</p>
-                <p>доолжность</p>
-                <p>Lorem dvsdvs dbhdbs </p>
-              </figcaption>
-            </figure>
-          </li>
-          <li className={styles.contactsListItem}>
-            <figure>
-              <img src="http://via.placeholder.com/280x245" alt="img" />
-              <figcaption>
-                <p>Имя фамилия</p>
-                <p>доолжность</p>
-                <p>Lorem dvsdvs dbhdbs </p>
-              </figcaption>
-            </figure>
-          </li>
-          <li className={styles.contactsListItem}>
-            <figure>
-              <img src="http://via.placeholder.com/280x245" alt="img" />
-              <figcaption>
-                <p>Имя фамилия</p>
-                <p>доолжность</p>
-                <p>Lorem dvsdvs dbhdbs </p>
-              </figcaption>
-            </figure>
-          </li>
+          {projectTeam.map(el => (
+            <li>
+              <ContactsListItem
+                name={el.name}
+                position={el.position}
+                description={el.description}
+              />
+            </li>
+          ))}
         </ul>
       </div>
     </section>
---
+++ src/pages/ContactsPage/ProjectTeam.js
@@ -0,0 +1,33 @@
+const projectTeam = [
+  {
+    name: 'Sergey Poddubnii',
+    position: 'Фронтенд разработчик',
+    description:
+      'Lorem Ipsum является стандартной рыбой для текстов на латиниц с начала  XVI века',
+  },
+  {
+    name: 'Denys Plietnikov',
+    position: 'Фронтенд разработчик',
+    description:
+      'Lorem Ipsum является стандартной рыбой для текстов на латиниц с начала  XVI века',
+  },
+  {
+    name: 'Коля Левків',
+    position: 'Фронтенд разработчик',
+    description:
+      'Lorem Ipsum является стандартной рыбой для текстов на латиниц с начала  XVI века',
+  },
+  {
+    name: 'Sergii Stryzh',
+    position: 'Фронтенд разработчик',
+    description:
+      'Lorem Ipsum является стандартной рыбой для текстов на латиниц с начала  XVI века',
+  },
+  {
+    name: 'Олег Галат',
+    position: 'Фронтенд разработчик',
+    description:
+      'Lorem Ipsum является стандартной рыбой для текстов на латиниц с начала  XVI века',
+  },
+];
+export default projectTeam;
