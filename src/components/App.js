308f747b3ed05cd62e1d5cd89601a96532d7aa15
Sergii Stryzg <strizgsergey@gmail.com>
2020-02-04 22:05:15 +0200

    add persistor

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import routes from '../routes';
+import authOperations from '../redux/auth/authOperations';
 /* import - components and pages */
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
@@ -13,30 +15,44 @@ import ContactsPage from '../pages/ContactsPage/ContactsPage';
 import Loader from './Loader/Loader';
 import PrivateRoute from '../servises/PrivateRoute';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Loader />
-      <Header />
+class App extends Component {
+  componentDidMount() {
+    const { getCurrentUser } = this.props;
+    getCurrentUser();
+  }
 
-      <Switch>
-        <Route path={routes.AUTH_PAGE} component={AuthPage} />
-        <PrivateRoute
-          exact
-          path={routes.DASHBOARD_PAGE}
-          component={DashboardPage}
-        />
-        <PrivateRoute path={routes.RESULT_PAGE} component={ResultPage} />
-        <PrivateRoute path={routes.MATERIALS_PAGE} component={MaterialsPage} />
-        <PrivateRoute path={routes.MAIN_PAGE} component={MainPage} />
-        <Route path={routes.CONTACTS_PAGE} component={ContactsPage} />
+  render() {
+    return (
+      <BrowserRouter>
+        <Loader />
+        <Header />
 
-        <Redirect to={routes.MAIN_PAGE} />
-      </Switch>
+        <Switch>
+          <Route path={routes.AUTH_PAGE} component={AuthPage} />
+          <PrivateRoute
+            exact
+            path={routes.DASHBOARD_PAGE}
+            component={DashboardPage}
+          />
+          <PrivateRoute path={routes.RESULT_PAGE} component={ResultPage} />
+          <PrivateRoute
+            path={routes.MATERIALS_PAGE}
+            component={MaterialsPage}
+          />
+          <PrivateRoute path={routes.MAIN_PAGE} component={MainPage} />
+          <Route path={routes.CONTACTS_PAGE} component={ContactsPage} />
 
-      <Footer />
-    </BrowserRouter>
-  );
-};
+          <Redirect to={routes.MAIN_PAGE} />
+        </Switch>
 
-export default App;
+        <Footer />
+      </BrowserRouter>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  getCurrentUser: () => dispatch(authOperations.getCurrentUser()),
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import './stylesheet/main.css';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import App from './components/App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
