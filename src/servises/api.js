b1d711f5fcd89a718b39ddae82e9121d2984cebf
Sergii Stryzg <strizgsergey@gmail.com>
2020-02-05 17:55:53 +0200

    small fix

--- src/servises/api.js
+++ src/servises/api.js
@@ -6,16 +6,16 @@ axios.defaults.headers.get['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.put['Content-Type'] = 'application/json';
 
-const sendResultRequest = (result, examId) => {
-  return axios.put(`/exams/${examId}/questions`, result);
-};
-
 export const setToken = token => {
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 };
 
 export const unsetToken = () => {
   axios.defaults.headers.common['Authorization'] = '';
+};
+
+const sendResultRequest = (result, examId) => {
+  return axios.put(`/exams/${examId}/questions`, result);
 };
 
 const getResultsStatus = examId => {
