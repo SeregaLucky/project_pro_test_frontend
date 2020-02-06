60f4836d1155777da4a74c32e841d2e23f58cc9c
Sergii Stryzg <strizgsergey@gmail.com>
2020-02-06 17:26:50 +0200

    small fix

--- src/redux/global/globalReducer.js
+++ src/redux/global/globalReducer.js
@@ -8,8 +8,8 @@ const loadingReducer = (state = false, { type, payload }) => {
     case questionsTypes.RESULTS_FINISHED_START:
       return true;
     case questionsTypes.RESULTS_SUCCESS:
-    case questionsTypes.RESULTS_FINISHED_SUCCESS:
     case questionsTypes.RESULTS_FAILURE:
+    case questionsTypes.RESULTS_FINISHED_SUCCESS:
     case questionsTypes.RESULTS_FINISHED_FAILURE:
       return false;
     default:
