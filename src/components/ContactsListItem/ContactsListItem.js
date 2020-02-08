36be8d0251e8a348a8c2a8140a18dc272d83fe20
Sergii Stryzg <strizgsergey@gmail.com>
2020-02-08 12:34:57 +0200

    fix small css in contactsPage

--- src/components/ContactsListItem/ContactsListItem.js
+++ src/components/ContactsListItem/ContactsListItem.js
@@ -1,29 +1,31 @@
 import React from 'react';
+import T from 'prop-types';
 import styles from './ContactsListItem.module.css';
-import cardImage from '../../assets/images/cardimage.jpg';
 
-const ContactsListItem = ({ image, name, nameRu, position, description }) => {
-  return (
-    <li className={styles.card}>
-      <figure>
-        <img src={image} className={styles.image} alt={name} />
-        <figcaption>
-          <p className={styles.name}>{name}</p>
-          <p className={styles.nameRu}>{nameRu}</p>
-          <p className={styles.position}>{position}</p>
-          <p className={styles.description}>{description}</p>
-        </figcaption>
-      </figure>
-    </li>
-  );
-};
+const ContactsListItem = ({ image, name, nameRu, position, eMail }) => (
+  <li className={styles.card}>
+    <figure>
+      <img src={image} className={styles.image} alt={name} height="246" />
+      <figcaption className={styles.figcaption}>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.nameRu}>{nameRu}</p>
+        <p className={styles.position}>{position}</p>
+        <p className={styles.eMail}>{eMail}</p>
+      </figcaption>
+    </figure>
+  </li>
+);
 
 ContactsListItem.defaultProps = {
-  image: cardImage,
-  name: 'Имя Фамилия',
-  position: 'Frontend developer',
-  description:
-    'Lorem Ipsum является стандартной "рыбой" для текстов на латиниц с начала  XVI века',
+  image: null,
+};
+
+ContactsListItem.propTypes = {
+  image: T.string,
+  name: T.string.isRequired,
+  nameRu: T.string.isRequired,
+  position: T.string.isRequired,
+  eMail: T.string.isRequired,
 };
 
 export default ContactsListItem;
--- src/pages/ContactsPage/ContactsPage.js
+++ src/pages/ContactsPage/ContactsPage.js
@@ -2,27 +2,25 @@ import React from 'react';
 import styles from './ContactsPage.module.css';
 import ContactsListItem from '../../components/ContactsListItem/ContactsListItem';
 import projectTeam from './ProjectTeam';
-import shortid from 'shortid';
-const ContactsPage = () => {
-  return (
-    <section className={styles.wrapper}>
-      <div className={styles.container}>
-        <h2 className={styles.mainTitle}>Наша команда</h2>
-        <ul className={styles.contactsListItemContainer}>
-          {projectTeam.map(el => (
-            <ContactsListItem
-              key={shortid.generate()}
-              name={el.name}
-              nameRu={el.nameRu}
-              position={el.position}
-              description={el.description}
-              image={el.imgSrc}
-            />
-          ))}
-        </ul>
-      </div>
-    </section>
-  );
-};
+
+const ContactsPage = () => (
+  <section className={styles.section}>
+    <div className={styles.container}>
+      <h2 className={styles.mainTitle}>Наша команда</h2>
+      <ul className={styles.contactsListItemContainer}>
+        {projectTeam.map(el => (
+          <ContactsListItem
+            key={el.id}
+            name={el.name}
+            nameRu={el.nameRu}
+            position={el.position}
+            eMail={el.eMail}
+            image={el.imgSrc}
+          />
+        ))}
+      </ul>
+    </div>
+  </section>
+);
 
 export default ContactsPage;
