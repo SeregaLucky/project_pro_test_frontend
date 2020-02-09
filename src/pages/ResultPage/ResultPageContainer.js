44d45cd5930af2b1035ab34e9d3070bf82a45371
OlhaLu <olhalutsyshyna@gmail.com>
2020-02-09 18:11:43 +0200

    Added propTypes in ResultPage and ResultContainer

--- src/pages/ResultPage/ResultPageContainer.js
+++ src/pages/ResultPage/ResultPageContainer.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import T from 'prop-types';
 import { connect } from 'react-redux';
 import ResultPage from './ResultPage.js';
 import * as selects from '../../redux/questions/questionsSelectors';
 import resultsOperations from '../../redux/questions/questionsOperations.js';
 
 class ResultPageContainer extends Component {
+  static propTypes = {
+    putFinished: T.func.isRequired,
+    getResults: T.func.isRequired,
+    finished: T.bool.isRequired,
+    result: T.shape({
+      answeredRight: T.number.isRequired,
+      answeredWrong: T.number.isRequired,
+    }).isRequired,
+  };
+
   componentDidMount() {
     const { putFinished } = this.props;
     putFinished();
@@ -28,7 +39,7 @@ class ResultPageContainer extends Component {
             answeredWrong={result.answeredWrong}
           />
         )}
-        {!result && <div>Not Results yet!</div>}
+        {!result && <div> Not Results yet </div>}
       </>
     );
   }
