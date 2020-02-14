06295aa7e2937a98baa95ca5c75558e87aa65f8f
OlhaLu <olhalutsyshyna@gmail.com>
2020-02-14 13:16:31 +0200

    Optimization into React Hooks, added useSelector, useDispatch, refactored code

--- src/pages/ResultPage/ResultPageContainer.js
+++ src/pages/ResultPage/ResultPageContainer.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import T from 'prop-types';
+import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import routes from '../../routes';
 import ResultPage from './ResultPage.js';
 import selects from '../../redux/questions/questionsSelectors';
@@ -9,72 +8,42 @@ import resultsOperations from '../../redux/questions/questionsOperations.js';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-class ResultPageContainer extends Component {
-  static defaultProps = {
-    result: null,
-    getIsResultSended: null,
-    error: null,
-  };
+const ResultPageContainer = () => {
+  const finished = useSelector(state => selects.getFinishedResults(state));
+  const result = useSelector(state => selects.getResults(state));
+  const isResultSended = useSelector(state => selects.getIsResultSended(state));
+  const error = useSelector(state => selects.getError(state));
 
-  static propTypes = {
-    putFinished: T.func.isRequired,
-    getResults: T.func.isRequired,
-    getIsResultSended: T.shape({}),
-    finished: T.bool.isRequired,
-    result: T.shape({
-      answeredRight: T.number.isRequired,
-      answeredWrong: T.number.isRequired,
-    }),
-    error: T.string,
-  };
+  const dispatch = useDispatch();
 
-  componentDidMount() {
-    const { putFinished } = this.props;
-    putFinished();
-  }
+  useEffect(() => {
+    dispatch(resultsOperations.putResultsFinished());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  componentDidUpdate(prevProps) {
-    const { finished, getResults, error } = this.props;
-    if (prevProps.finished !== finished) {
+  useEffect(() => {
+    if (finished) {
       toast.success('Update test result success', { autoClose: 2000 });
-      getResults();
-    } else if (error) {
-      toast.error('Error with server, try again later!', { autoClose: false });
-      return;
+      dispatch(resultsOperations.getResultsById());
     }
-  }
+    if (error) {
+      toast.error('Error with server, try again later!', { autoClose: false });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [finished]);
 
-  render() {
-    const { result, isResultSended } = this.props;
+  return (
+    <>
+      {result && (
+        <ResultPage
+          answeredRight={result.answeredRight}
+          answeredWrong={result.answeredWrong}
+        />
+      )}
+      {!isResultSended && <Redirect to={routes.MAIN_PAGE} />}
+      <ToastContainer />
+    </>
+  );
+};
 
-    return (
-      <>
-        {result && (
-          <ResultPage
-            answeredRight={result.answeredRight}
-            answeredWrong={result.answeredWrong}
-          />
-        )}
-        {!isResultSended && <Redirect to={routes.MAIN_PAGE} />}
-        <ToastContainer />
-      </>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  finished: selects.getFinishedResults(state),
-  result: selects.getResults(state),
-  isResultSended: selects.getIsResultSended(state),
-  error: selects.getError(state),
-});
-
-const mapDispatchToProps = dispatch => ({
-  putFinished: () => dispatch(resultsOperations.putResultsFinished()),
-  getResults: () => dispatch(resultsOperations.getResultsById()),
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(ResultPageContainer);
+export default ResultPageContainer;
