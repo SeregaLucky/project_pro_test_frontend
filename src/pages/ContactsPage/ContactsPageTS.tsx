c119d37efd6d2368da0e413c84898c217ea5a2ad
Manuilenkoart <manuilenkoart@gmail.com>
2020-02-16 12:14:36 +0200

    init contactsPageTypeScript branch

---
+++ src/pages/ContactsPage/ContactsPageTS.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import  './ContactsPage.css';
+import ContactsListItemTS from '../../components/ContactsListItem/ContactsListItemTS';
+import projectTeam from './ProjectTeam.js';
+
+
+const ContactsPage:React.FC =()=> (
+  <section className='section'>
+    <div className='container'>
+      <h2 className='mainTitle'>Наша команда</h2>
+      <ul className='contactsListItemContainer'>
+        {projectTeam.map(el => (
+          <ContactsListItemTS
+            key={el.id}
+            name={el.name}
+            nameRu={el.nameRu}
+            position={el.position}
+            eMail={el.eMail}
+            image={el.imgSrc}
+          />
+        ))}
+      </ul>
+    </div>
+  </section>
+);
+
+export default ContactsPage;
