4cc08120c70f3b12f39f764506a7f87e585e388f
OlhaLu <olhalutsyshyna@gmail.com>
2020-02-16 21:31:32 +0200

    Pull dev changes

--- src/pages/ContactsPage/ContactsPageTS.tsx
+++ src/pages/ContactsPage/ContactsPageTS.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import  './ContactsPage.css';
+import './ContactsPage.css';
 import ContactsListItemTS from '../../components/ContactsListItem/ContactsListItemTS';
 import projectTeamTS from './projectTeamTS';
 
-
-const ContactsPage:React.FC =()=> (
-  <section className='section'>
-    <div className='container'>
-      <h2 className='mainTitle'>Наша команда</h2>
-      <ul className='contactsListItemContainer'>
+const ContactsPage: React.FC = () => (
+  <section className="section">
+    <div className="container">
+      <h2 className="mainTitle">Наша команда</h2>
+      <ul className="contactsListItemContainer">
         {projectTeamTS.map(el => (
           <ContactsListItemTS
             key={el.id}
